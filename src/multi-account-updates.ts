52dfb15918db72076242ab3ecdab5c544a4cb2d7
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2023-08-24 16:13:44 +0300

    Add options to loads

--- src/multi-account-updates.ts
+++ src/multi-account-updates.ts
@@ -1,39 +1,98 @@
-import { Command } from '@commander-js/extra-typings';
+import { Command, Option, OptionValues } from '@commander-js/extra-typings';
 import { AccountUpdate, PrivateKey, PublicKey, UInt64 } from 'snarkyjs';
 import { Logger } from 'tslog';
 import { LoadDescriptor, TransactionData } from './load-descriptor.js';
 import { LoadRegistry } from './load-registry.js';
 import { LOG } from './log.js';
 import { MultiAcc } from './MultiAcc.js';
+import { myParseAccounts, myParseMina } from './parse-int.js';
 
 export interface Transfer {
   to: PublicKey;
   amount: number;
 }
 
+interface SimpleOpts extends OptionValues {
+  receivers: PublicKey[];
+  amount: number;
+  fee: number;
+}
+
+interface ZkappOpts extends SimpleOpts {
+  initialAmount: number;
+}
+
+function accounts() {
+  return new Option(
+    '-r, --receivers <public-key...>',
+    'accounts to transfer funds to'
+  )
+    .argParser(myParseAccounts)
+    .default([]);
+}
+
+function amount() {
+  return new Option('-a, --amount <mina>', 'amount to transfer')
+    .argParser(myParseMina)
+    .default(10e9);
+}
+
+function initialAmount() {
+  return new Option(
+    '-i, --initial-amount <mina>',
+    'inital amount to fund zkapp account with'
+  )
+    .argParser(myParseMina)
+    .default(1000e9);
+}
+
+function fee() {
+  return new Option('-f, --fee <mina>', 'amount of fee')
+    .argParser(myParseMina)
+    .default(1e9);
+}
+
+function simpleCommand(
+  name: string,
+  description?: string
+): () => Command<[], SimpleOpts> {
+  return () =>
+    new Command(name)
+      .description(description || '')
+      .addOption(accounts())
+      .addOption(amount())
+      .addOption(fee());
+}
+
+function zkappCommand(
+  name: string,
+  description?: string
+): () => Command<[], ZkappOpts> {
+  return () =>
+    new Command(name)
+      .description(description || '')
+      .addOption(accounts())
+      .addOption(amount())
+      .addOption(initialAmount())
+      .addOption(fee());
+}
+
 abstract class MultiAccTrans implements LoadDescriptor {
   log: Logger<any>;
   sender: PublicKey;
-  a1: PublicKey;
-  a2: PublicKey;
-  a3: PublicKey;
-  a4: PublicKey;
-  a5: PublicKey;
-  a6: PublicKey;
-  a7: PublicKey;
-  a8: PublicKey;
+  accounts: PublicKey[];
+  amount: number;
+  fee: number;
 
-  constructor() {
+  constructor(opts: SimpleOpts) {
     this.log = LOG.getSubLogger({ name: 'matp' });
+    this.accounts = opts.receivers;
+    this.amount = opts.amount;
+    this.fee = opts.fee;
 
-    this.a1 = PrivateKey.random().toPublicKey();
-    this.a2 = PrivateKey.random().toPublicKey();
-    this.a3 = PrivateKey.random().toPublicKey();
-    this.a4 = PrivateKey.random().toPublicKey();
-    this.a5 = PrivateKey.random().toPublicKey();
-    this.a6 = PrivateKey.random().toPublicKey();
-    this.a7 = PrivateKey.random().toPublicKey();
-    this.a8 = PrivateKey.random().toPublicKey();
+    this.log.debug('accounts:', this.accounts);
+    this.log.debug('amount', this.amount);
+    this.log.debug('fee:', this.fee);
   }
 
   getSetupTransaction(
@@ -45,7 +104,7 @@ abstract class MultiAccTrans implements LoadDescriptor {
 
   getTransaction(_account: PublicKey) {
     return Promise.resolve({
-      fee: 10e9,
+      fee: this.fee,
       signers: this.signers(),
       body: this.body(),
     });
@@ -61,12 +120,14 @@ abstract class MultiAccTrans implements LoadDescriptor {
 abstract class MultiAccWithZkApp extends MultiAccTrans {
   zk: MultiAcc;
   zkKey: PrivateKey;
+  initialAmount: number;
 
-  constructor() {
-    super();
+  constructor(opts: ZkappOpts) {
+    super(opts);
 
     this.zkKey = PrivateKey.random();
     this.zk = new MultiAcc(this.zkKey.toPublicKey());
+    this.initialAmount = opts.initialAmount;
 
     this.log.info(`zkApp public key: ${this.zkKey.toPublicKey().toBase58()}`);
     this.log.debug(`zkApp private key: ${this.zkKey.toBase58()}`);
@@ -80,10 +141,13 @@ abstract class MultiAccWithZkApp extends MultiAccTrans {
     return {
       body: () => {
         let update = AccountUpdate.fundNewAccount(account);
-        update.send({ to: this.zkKey.toPublicKey(), amount: 100e9 });
+        update.send({
+          to: this.zkKey.toPublicKey(),
+          amount: this.initialAmount,
+        });
         this.zk.deploy();
       },
-      fee: 10e9,
+      fee: this.fee,
       signers: [this.zkKey],
     };
   }
@@ -93,227 +157,222 @@ class Simple1 extends MultiAccTrans {
   body() {
     return () => {
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
-        amount: UInt64.from(1000e9),
+        to: this.accounts[0],
+        amount: UInt64.from(this.amount),
       });
     };
   }
 }
-LoadRegistry.register(Simple1, () => new Command('simple1').description(''));
+LoadRegistry.register(Simple1, simpleCommand('simple1'));
 
 class Simple2 extends MultiAccTrans {
   body() {
     return () => {
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
-        amount: UInt64.from(10e9),
+        to: this.accounts[0],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a2,
-        amount: UInt64.from(10e9),
+        to: this.accounts[1],
+        amount: UInt64.from(this.amount),
       });
     };
   }
 }
-LoadRegistry.register(Simple2, () => new Command('simple2').description(''));
+LoadRegistry.register(Simple2, simpleCommand('simple2', ''));
 
 class Simple4 extends MultiAccTrans {
   body() {
     return () => {
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
-        amount: UInt64.from(10e9),
+        to: this.accounts[0],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a2,
-        amount: UInt64.from(10e9),
+        to: this.accounts[1],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a3,
-        amount: UInt64.from(10e9),
+        to: this.accounts[2],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a4,
-        amount: UInt64.from(10e9),
+        to: this.accounts[3],
+        amount: UInt64.from(this.amount),
       });
     };
   }
 }
-LoadRegistry.register(Simple4, () => new Command('sign-x4'));
+LoadRegistry.register(Simple4, simpleCommand('sign-x4'));
 
 class Simple8 extends MultiAccTrans {
   body() {
     return () => {
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
-        amount: UInt64.from(10e9),
+        to: this.accounts[0],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a2,
-        amount: UInt64.from(10e9),
+        to: this.accounts[1],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a3,
-        amount: UInt64.from(10e9),
+        to: this.accounts[2],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a4,
-        amount: UInt64.from(10e9),
+        to: this.accounts[3],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a5,
-        amount: UInt64.from(10e9),
+        to: this.accounts[4],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a6,
-        amount: UInt64.from(10e9),
+        to: this.accounts[5],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a7,
-        amount: UInt64.from(10e9),
+        to: this.accounts[6],
+        amount: UInt64.from(this.amount),
       });
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a8,
-        amount: UInt64.from(10e9),
+        to: this.accounts[7],
+        amount: UInt64.from(this.amount),
       });
     };
   }
 }
-LoadRegistry.register(Simple8, () => new Command('simple8').description(''));
+LoadRegistry.register(Simple8, simpleCommand('simple8'));
 
 class Simple4AndZkApp4 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(30e9));
+      this.zk.deposit(UInt64.from(this.amount * 4));
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
-        amount: UInt64.from(10e9),
+        to: this.accounts[0],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a2);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[1]);
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a3,
-        amount: UInt64.from(10e9),
+        to: this.accounts[2],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a4);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[3]);
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a5,
-        amount: UInt64.from(10e9),
+        to: this.accounts[4],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a6);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[5]);
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a7,
-        amount: UInt64.from(10e9),
+        to: this.accounts[6],
+        amount: UInt64.from(this.amount),
       });
+      //this.zk.transfer(UInt64.from(this.amount), this.accounts[7]);
     };
   }
 }
-LoadRegistry.register(Simple4AndZkApp4, () =>
-  new Command('simple4-zkapp4').description('')
-);
+LoadRegistry.register(Simple4AndZkApp4, zkappCommand('simple4-zkapp4'));
 
 class ZkApp4AndSimple4 extends MultiAccWithZkApp {
   body() {
     return () => {
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a1,
+        to: this.accounts[0],
         amount: UInt64.from(10e9),
       });
-      this.zk.deposit(UInt64.from(30e9));
+      this.zk.deposit(UInt64.from(this.amount * 3));
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a3,
-        amount: UInt64.from(10e9),
+        to: this.accounts[1],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a2);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[2]);
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a5,
-        amount: UInt64.from(10e9),
+        to: this.accounts[3],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a4);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[4]);
       AccountUpdate.createSigned(this.sender).send({
-        to: this.a7,
-        amount: UInt64.from(10e9),
+        to: this.accounts[5],
+        amount: UInt64.from(this.amount),
       });
-      this.zk.transfer(UInt64.from(10e9), this.a6);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[6]);
     };
   }
 }
-LoadRegistry.register(ZkApp4AndSimple4, () =>
-  new Command('zkapp4-simple4').description('')
-);
+LoadRegistry.register(ZkApp4AndSimple4, zkappCommand('zkapp4-simple4'));
 
 class ZkApp1 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(10e9));
+      this.zk.deposit(UInt64.from(this.amount));
     };
   }
 }
-LoadRegistry.register(ZkApp1, () => new Command('zkapp1').description(''));
+LoadRegistry.register(ZkApp1, zkappCommand('zkapp1'));
 
 class ZkApp2 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(100e9));
-      this.zk.transfer(UInt64.from(50e9), this.a1);
+      this.zk.deposit(UInt64.from(this.amount));
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[0]);
     };
   }
 }
-LoadRegistry.register(ZkApp2, () => new Command('zkapp2').description(''));
+LoadRegistry.register(ZkApp2, zkappCommand('zkapp2'));
 
 class ZkApp3 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(20e9));
-      this.zk.transfer(UInt64.from(10e9), this.a1);
-      this.zk.transfer(UInt64.from(10e9), this.a2);
+      this.zk.deposit(UInt64.from(this.amount * 2));
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[0]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[1]);
     };
   }
 }
-LoadRegistry.register(ZkApp3, () =>
-  new Command('sign-proof-x3').description('')
-);
+LoadRegistry.register(ZkApp3, zkappCommand('sign-proof-x3'));
 
 class ZkApp4 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(30e9));
-      this.zk.transfer(UInt64.from(10e9), this.a1);
-      this.zk.transfer(UInt64.from(10e9), this.a2);
-      this.zk.transfer(UInt64.from(10e9), this.a3);
+      this.zk.deposit(UInt64.from(this.amount * 3));
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[0]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[1]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[2]);
     };
   }
 }
-LoadRegistry.register(ZkApp4, () => new Command('zkapp4').description(''));
+LoadRegistry.register(ZkApp4, zkappCommand('zkapp4'));
 
 class ZkApp8 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(70e9));
-      this.zk.transfer(UInt64.from(10e9), this.a1);
-      this.zk.transfer(UInt64.from(10e9), this.a2);
-      this.zk.transfer(UInt64.from(10e9), this.a3);
-      this.zk.transfer(UInt64.from(10e9), this.a4);
-      this.zk.transfer(UInt64.from(10e9), this.a5);
-      this.zk.transfer(UInt64.from(10e9), this.a6);
-      this.zk.transfer(UInt64.from(10e9), this.a7);
+      this.zk.deposit(UInt64.from(this.amount * 7));
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[0]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[1]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[2]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[3]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[4]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[5]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[6]);
     };
   }
 }
-LoadRegistry.register(ZkApp8, () => new Command('zkapp8').description(''));
+LoadRegistry.register(ZkApp8, zkappCommand('zkapp8'));
 
 class ZkApp9 extends MultiAccWithZkApp {
   body() {
     return () => {
-      this.zk.deposit(UInt64.from(80e9));
-      this.zk.transfer(UInt64.from(10e9), this.a1);
-      this.zk.transfer(UInt64.from(10e9), this.a2);
-      this.zk.transfer(UInt64.from(10e9), this.a3);
-      this.zk.transfer(UInt64.from(10e9), this.a4);
-      this.zk.transfer(UInt64.from(10e9), this.a5);
-      this.zk.transfer(UInt64.from(10e9), this.a6);
-      this.zk.transfer(UInt64.from(10e9), this.a7);
-      this.zk.transfer(UInt64.from(10e9), this.a8);
+      this.zk.deposit(UInt64.from(this.amount * 8));
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[0]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[1]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[2]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[3]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[4]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[5]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[6]);
+      this.zk.transfer(UInt64.from(this.amount), this.accounts[7]);
     };
   }
 }
-LoadRegistry.register(ZkApp9, () => new Command('zkapp9').description(''));
+LoadRegistry.register(ZkApp9, zkappCommand('zkapp9'));
--- src/parse-int.ts
+++ src/parse-int.ts
@@ -1,7 +1,7 @@
 import { InvalidArgumentError } from '@commander-js/extra-typings';
+import { PublicKey } from 'snarkyjs';
 
 export function myParseMina(value: string, _previous: number) {
-  // parseInt takes a string and a radix
   const parsedValue = parseFloat(value);
   if (isNaN(parsedValue)) {
     throw new InvalidArgumentError('Not a number.');
@@ -16,4 +16,21 @@ export function myParseInt(value: string, _previous: number) {
     throw new InvalidArgumentError('Not a number.');
   }
   return parsedValue;
+}
+
+export function _myParseAccount(value: string) {
+  try {
+    return PublicKey.fromBase58(value);
+  } catch {
+    throw new InvalidArgumentError(`Not an account: {value}`);
+  }
+}
+
+export function myParseAccount(value: string, _previous: PublicKey) {
+  return _myParseAccount(value);
+}
+
+export function myParseAccounts(value: string, previous: PublicKey[]) {
+  console.log('>>> ', value);
+  return previous.concat(_myParseAccount(value));
 }
